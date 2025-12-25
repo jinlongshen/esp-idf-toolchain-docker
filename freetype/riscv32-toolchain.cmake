899ac1f7b9692501c9a4c903cb4a45e0b57b1d5a
Jinlong Shen <shenjinlong@yahoo.com>
2025-12-25 03:23:11 +0100

    corrected toolchain paths

--- freetype/riscv32-toolchain.cmake
+++ freetype/riscv32-toolchain.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv32)
 
-# Absolute paths required by CMake
-set(CMAKE_C_COMPILER   /opt/esp/idf-tools/tools/riscv32-esp-elf/bin/riscv32-esp-elf-gcc)
-set(CMAKE_CXX_COMPILER /opt/esp/idf-tools/tools/riscv32-esp-elf/bin/riscv32-esp-elf-g++)
+# Absolute paths to the compiler inside the base image
+set(CMAKE_C_COMPILER   /root/.espressif/tools/riscv32-esp-elf/esp-15.2.0_20251204/riscv32-esp-elf/bin/riscv32-esp-elf-gcc)
+set(CMAKE_CXX_COMPILER /root/.espressif/tools/riscv32-esp-elf/esp-15.2.0_20251204/riscv32-esp-elf/bin/riscv32-esp-elf-g++)
 
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 
