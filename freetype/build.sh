a3e5e7e88640c8d9f2ffe557e28ecab56bb8b835
Jinlong Shen <shenjinlong@yahoo.com>
2026-01-02 21:19:13 +0100

    moved toolchain file for shared use

--- freetype/build.sh
+++ freetype/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Configure FreeType from the upstream directory
 cmake -B build -S freetype \
   -G Ninja \
-  -DCMAKE_TOOLCHAIN_FILE=$(pwd)/riscv32-toolchain.cmake \
+  -DCMAKE_TOOLCHAIN_FILE=/opt/toolchain/riscv32-toolchain.cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/opt/freetype
 
-# Build
 cmake --build build
-
-# Install
 cmake --install build
 
