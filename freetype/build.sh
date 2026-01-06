2568f2aa584a62a8ecb88004b66a649cf59cd7c3
Jinlong Shen <shenjinlong@yahoo.com>
2026-01-06 14:10:04 +0100

    fixing to use native toolchain file of esp-idf

--- freetype/build.sh
+++ freetype/build.sh
@@ -3,10 +3,11 @@ set -e
 
 cmake -B build -S freetype \
   -G Ninja \
-  -DCMAKE_TOOLCHAIN_FILE=/opt/toolchain/riscv32-toolchain.cmake \
+  -DCMAKE_TOOLCHAIN_FILE=/opt/esp-idf/tools/cmake/toolchain-esp32c3.cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=/opt/freetype
 
 cmake --build build
 cmake --install build
+
 
